8d4cecd9d791fecd091579063d081cc138f6493c
Brandon Rivera <brandonviolin@gmail.com>
2018-09-27 12:37:56 -0700

    Update with models

---
+++ AuctionSiteProject/AuctionSiteProject/Models/Auction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuctionSiteProject.Models
+{
+    public class Auction
+    {
+        public int auctionID { get; set; }
+        public string auctionType { get; set; }
+    }
+}
